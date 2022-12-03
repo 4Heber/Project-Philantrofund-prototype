ab0a773cc691d8f31b79888c5064657e0822ca39
4Heber <parzival.html@gmail.com>
2022-12-03 20:52:12 +0100

    Main title with animation 'Typed.js', searchbar change form on menu active

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,64 @@
-const menuBttn = document.getElementsByClassName('burger-menu');
-const iconBttn = menuBttn[0].children.item(0);
+
+const menuBttn = document.getElementById('burger-menu');
 const asideNav = document.getElementById('aside-nav');
 const h1Hero = document.getElementsByClassName('title-container');
 const svgHero = document.getElementsByClassName('illustration-box');
+const searchBar = document.getElementsByClassName('search-container');
 
-// Fade out title h1 && illustration
-// function fadeOut(){
+// Menu button on click animations
+menuBttn.addEventListener('click', () => {
 
-//     var intervalID = setInterval(() => {
-        
-//         console.log(!h1Hero[0].style.opacity, !svgHero[0].style.opacity)
-
-//         if((h1Hero[0].style.opacity = 0) && (svgHero[0].style.opacity = 0)){
-//             clearInterval(intervalID);
-//         }
-
-//         if((h1Hero[0].style.opacity && svgHero[0].style.opacity) > 0){
-//             h1Hero[0].style.opacity -= 0.1;
-//             svgHero[0].style.opacity -= 0.1;
-//         }
-
-//     }, 30);
-// }
-
-menuBttn[0].addEventListener('click', () => {
-
-    // Change menu icon
-    if(asideNav.classList.contains('slide-out')){
-        iconBttn.classList.replace('fa-bars', 'fa-times');
-        asideNav.classList.replace('slide-out','slide-in');
-
-    }
-    else{
-        iconBttn.classList.replace('fa-times', 'fa-bars');
-        asideNav.classList.replace('slide-in', 'slide-out');
-    }
-    
-    // fadeOut();
+    // Change menu form cross-burger
+    menuBttn.classList.toggle('changeMenuForm');
     
     // Hidden title & illustration
     h1Hero[0].classList.toggle('hidden');
     svgHero[0].classList.toggle('hidden');
+
+    // Slide in-out aside nav
+    asideNav.classList.toggle('slide-out');
+
+    // Change search-bar form
+    searchBar[0].classList.toggle('search-menu-active');
+
 })
+
+
+// Main title with Typed.js animation
+var typed = new Typed('.typed',{
+
+    stringsElement: '#typed-strings',
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contetType: 'html'
+});
+
+
+// Go top button function
+let goTopBttn = document.getElementById('goTopBttn');
+
+window.onscroll = ()=>{
+    scrollFunction()
+};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        goTopBttn.style.display = 'block';
+
+    }else{
+        goTopBttn.style.display = 'none';
+    }
+};
+
+function goTopFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
